3637f26f551e1900048a9df364f2a0b06b56ac80
harukipull <harukigit@gmail.com>
2020-01-18 16:31:15 +0900

    add view files and actions

--- app/controllers/expense_controller.rb
+++ app/controllers/expense_controller.rb
@@ -1,2 +1,21 @@
 class ExpenseController < ApplicationController
+
+
+    def index
+    end
+
+    def new
+    end
+
+    def create
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def destroy
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/fork'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  root 'static_pages#home'
+  get  '/fork' ,to: 'static_pages#fork'
+  resources :expenses
+  resources :incomes
 end
