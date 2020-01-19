edbdef563d33ff1a63bf118ca87894150b661287
harukipull <harukigit@gmail.com>
2020-01-19 22:49:33 +0900

    apply bootstrap for incomes/new.html.erb

--- app/models/income.rb
+++ app/models/income.rb
@@ -1,2 +1,6 @@
 class Income < ApplicationRecord 
+    validates :category, presence:true
+    validates :price, presence:true
+    validates :date, presence:true
+    validates :memo, presence:true
 end
